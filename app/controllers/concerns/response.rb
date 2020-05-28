2a2910511a34e4e2d1218ac5512e5dd1be7340fe
Brandon Fonseca <brandonfonseca5@gmail.com>
2020-05-28 01:13:02 -0400

    remove views folder since it isn't needed

--- app/controllers/concerns/response.rb
+++ app/controllers/concerns/response.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
+# helper functions related to API response
 module Response
+  # simple function to return json
   def json_response(object, status = :ok)
     render json: object, status: status
   end
